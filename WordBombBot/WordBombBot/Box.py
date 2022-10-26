770ba415895445b7234051520fb9eaff1db2a3ad
Abdul <95952874+AbdulWasTaken@users.noreply.github.com>
2022-10-25 20:07:39 -0400

    Add files via upload

--- WordBombBot/WordBombBot/Box.py
+++ WordBombBot/WordBombBot/Box.py
@@ -10,8 +10,12 @@ class Box:
 
     def get_box(self):
         for i in range(1, 3):
-            print(f"Press [{i}] "
-                  "when cursor is on the upper-left corner of the text box")
+            if i == 1:
+                print(f"Press [{i}] "
+                      "when cursor is on the upper-left corner of the text box")
+            else:
+                print(f"Press [{i}] "
+                      "when cursor is on the bottom-right corner of the text box")
             while not keyboard.is_pressed(str(i)):
                 self.coords[i - 1] = pyautogui.position() # Returns pos as [(x,y), (x,y)]
 
